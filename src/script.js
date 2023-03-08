d50840d1cbaddaaf99640f92d274dd85da5a2752
emanuelerm <emanueler00@gmail.com>
2023-03-08 15:59:11 +0100

    code

--- src/script.js
+++ src/script.js
@@ -6,3 +6,32 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 */
+
+let passengersAge = document.getElementById(passengersAge).value;
+let distance = document.getElementById(distance).value;
+
+
+const priceForKm = 0.21;
+const discountUnder = -0.2;
+const discountOver = -0.4;
+let priceForDistance = distance * priceForKm
+
+
+let ticketPrice = '';
+
+
+if ((!isNaN == passengersAge) && (!isNaN == distance)) {
+    if (passengersAge >= 18 && passengersAge <= 65) {
+        ticketPrice = (passengersAge * priceForDistance).toFixed(2);
+    } else if (passengersAge <= 17) {
+        ticketPrice = (((passengersAge * priceForDistance) * discountUnder) + (passengersAge * priceForDistance)).toFixed(2);
+    } else if (passengersAge >= 66) {
+        ticketPrice = (((passengersAge * priceForDistance) * discountOver) + (passengersAge * priceForDistance)).toFixed(2);
+    } 
+
+    }   else {
+        alert('Insert the correct data')
+}
+
+
+console.log(ticketPrice);
